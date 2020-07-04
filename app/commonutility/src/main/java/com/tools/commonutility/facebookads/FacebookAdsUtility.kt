5b24d73ab1baf053e6aca5a1cf96d42cd97b1d9d
ankush.kapoor <ankush.kapoor@paytm.com>
2020-07-04 10:59:02 +0530

    fb

--- app/commonutility/src/main/java/com/tools/commonutility/facebookads/FacebookAdsUtility.kt
+++ app/commonutility/src/main/java/com/tools/commonutility/facebookads/FacebookAdsUtility.kt
@@ -5,8 +5,10 @@ import android.text.TextUtils
 import android.util.Log
 import android.widget.LinearLayout
 import com.facebook.ads.*
+import java.util.*
+import kotlin.collections.ArrayList
 
-class FacebookAdsUtility(private val context: Context, private val isDebugApp: Boolean, private val deviceIdHash: String?) {
+class FacebookAdsUtility(private val context: Context, private val isDebugApp: Boolean, private val deviceIdHash: String?, private val installer: String?) {
 
     init {
         AudienceNetworkAds.initialize(context)
@@ -16,6 +18,9 @@ class FacebookAdsUtility(private val context: Context, private val isDebugApp: B
     }
 
     fun loadFacebookBannerAdSize50(placementId: String, containerView: LinearLayout) {
+        if (!isDebugApp && !isAppDownloadFromPlayStore()) {
+            return
+        }
         var formattedPlacementId = placementId
         if (isDebugApp) {
             formattedPlacementId = "IMG_16_9_APP_INSTALL#$placementId"
@@ -46,5 +51,16 @@ class FacebookAdsUtility(private val context: Context, private val isDebugApp: B
             }
 
         })
+    }
+
+    fun isAppDownloadFromPlayStore(): Boolean {
+        // A list with valid installers package name
+        val validInstallers: List<String> = ArrayList(Arrays.asList("com.android.vending", "com.google.android.feedback"))
+
+        // The package name of the app that has installed your app
+        //val installer = context.packageManager.getInstallerPackageName(context.packageName)
+
+        // true if your app has been downloaded from Play Store
+        return !TextUtils.isEmpty(installer) && validInstallers.contains(installer)
     }
 }
